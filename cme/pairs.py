f31714c8df908fbac469a155bdbe1f770f8dadb2
oktrader <sailor09@protonmail.com>
2020-02-09 10:33:49 +0000

    CME pairs

--- cme/pairs.py
+++ cme/pairs.py
@@ -1,5 +1,8 @@
+import sys
 import argparse
+from string import ascii_uppercase
 import requests
+
 
 parser = argparse.ArgumentParser(description='TV tickers')
 parser.add_argument('-e', '--exchange')
@@ -8,6 +11,19 @@ parser.add_argument('-e', '--exchange')
 if __name__ == "__main__":
     args = parser.parse_args()
 
-    symbols = requests.get(f'https://symbol-search.tradingview.com/symbol_search/?exchange={args.exchange.upper()}&type=&hl=true&lang=en&domain=production').json()
-    symbols = map(lambda x: '{}:{}'.format(args.exchange.upper(), x['contracts'][0]['symbol']), symbols)
-    print(',\n'.join(symbols))
+    all_symbols = []
+    for c in ascii_uppercase:
+        symbols = requests.get(f'https://symbol-search.tradingview.com/symbol_search/', params={
+            'exchange': args.exchange.upper(),
+            'lang': 'en',
+            'domain': 'production',
+            'text': c
+        }).json()
+        symbols = list(map(lambda x: '{}:{}'.format(args.exchange.upper(), x['contracts'][0]['symbol']), symbols))
+
+        if len(symbols) >= 50:
+            print(f'{c} has more than 50 symbols', file=sys.stderr) # TODO fix this
+
+        all_symbols.extend(symbols)
+
+    print(',\n'.join(sorted(all_symbols)))
