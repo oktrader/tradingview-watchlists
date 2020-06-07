aeac4a68feee6a522decb3b8bfd981f8a8e42859
oktrader <oktrader>
2020-06-08 00:55:42 +0100

    Add ftx perps

--- ftx/pairs.py
+++ ftx/pairs.py
@@ -5,6 +5,7 @@ parser = argparse.ArgumentParser(description='FTX tickers')
 parser.add_argument('-q', '--quote-asset')
 parser.add_argument('-f', '--futures', action='store_true')
 parser.add_argument('-s', '--spot', action='store_true')
+parser.add_argument('-p', '--perps', action='store_true')
 
 
 if __name__ == "__main__":
@@ -16,6 +17,8 @@ if __name__ == "__main__":
         symbols = filter(lambda x: x['type'] == 'future', symbols)
     if args.spot:
         symbols = filter(lambda x: x['type'] == 'spot', symbols)
+    if args.perps:
+        symbols = filter(lambda x: x['type'] =='future' and x['name'].endswith('PERP'), symbols)
 
     symbols = map(lambda x: 'FTX:{}'.format(x['name'].upper().replace('-', '').replace('/', '')), symbols)
 
