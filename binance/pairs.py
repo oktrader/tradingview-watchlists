0647abed9eb581d1dafa736e065ffa959101de1b
oktrader <oktrader>
2020-07-06 15:43:01 +0100

    Add binance futures

--- binance/pairs.py
+++ binance/pairs.py
@@ -3,19 +3,21 @@ import requests
 
 parser = argparse.ArgumentParser(description='Binance tickers')
 parser.add_argument('-m', '--margin', action='store_true')
+parser.add_argument('-f', '--futures', action='store_true')
 parser.add_argument('-q', '--quote-asset')
 
 if __name__ == "__main__":
     args = parser.parse_args()
 
-    symbols = requests.get('https://api.binance.com/api/v1/exchangeInfo').json()['symbols']
-
-    if args.margin:
-        symbols = filter(lambda x: x['isMarginTradingAllowed'], symbols)
-    
-    if args.quote_asset:
-        symbols = filter(lambda x: x['quoteAsset'] == args.quote_asset, symbols)
-
-    symbols = map(lambda x: 'BINANCE:{}'.format(x['symbol']), symbols)
-
-    print(',\n'.join(sorted(symbols)))
+    if args.futures:
+        symbols = requests.get('https://fapi.binance.com/fapi/v1/exchangeInfo').json()['symbols']
+        symbols = map(lambda x: 'BINANCE:{}PERP'.format(x['symbol']), symbols)
+        print(',\n'.join(sorted(symbols)))
+    else:
+        symbols = requests.get('https://api.binance.com/api/v1/exchangeInfo').json()['symbols']
+        if args.margin:
+            symbols = filter(lambda x: x['isMarginTradingAllowed'], symbols)
+        if args.quote_asset:
+            symbols = filter(lambda x: x['quoteAsset'] == args.quote_asset, symbols)
+        symbols = map(lambda x: 'BINANCE:{}'.format(x['symbol']), symbols)
+        print(',\n'.join(sorted(symbols)))
